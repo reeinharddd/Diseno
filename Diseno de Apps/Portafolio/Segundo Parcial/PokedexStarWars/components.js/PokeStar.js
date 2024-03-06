7cc833d08593f4f220ec437311588c934df1e53c
Erik <beltraberik@gmail.com>
2024-03-06 11:26:57 -0800

    Poke api

--- Diseno de Apps/Portafolio/Segundo Parcial/PokedexStarWars/components.js/PokeStar.js
+++ Diseno de Apps/Portafolio/Segundo Parcial/PokedexStarWars/components.js/PokeStar.js
@@ -21,7 +21,7 @@ export default function PokeStar() {
       .then((response) => response.json())
       .then(
         (result) => {
-          setData(result);
+          setData(result.results);
           setIsLoading(false);
         },
         (error) => {
@@ -34,19 +34,64 @@ export default function PokeStar() {
   console.log(data);
   console.log(error);
 
-  return (
-    <View>
-      <Text style={styles.textProps}>Loading data</Text>
-      <ActivityIndicator
-        size='large'
-        color='#0000ff'
-      />
-    </View>
-  );
+  const getContent = () => {
+    if (isLoading) {
+      return (
+        <View>
+          <Text>Loading Data</Text>
+          <ActivityIndicator
+            size='large'
+            color='green'
+          />
+        </View>
+      );
+    }
+    if (error) {
+      return <Text>Error</Text>;
+    }
+    return (
+      <View>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <View style={styles.cardContainer}>
+              <Text>Name: {item.name}</Text>
+              <Text>Height: {item.height}</Text>
+              <Text>Weight: {item.mass}</Text>
+              <Text>Created: {item.created} </Text>
+              <Text>Edited: {item.edited} </Text>
+            </View>
+          )}
+        />
+      </View>
+    );
+  };
+
+  return <View>{getContent()}</View>;
 }
 const styles = StyleSheet.create({
   textProps: {
     fontSize: 20,
     color: "red",
+  },
+  cardContainer: {
+    backgroundColor: "lightblue",
+    borderRadius: 10,
+    padding: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    boxShadowColor: "black",
+    boxShadowOffset: { width: 0, height: 2 },
+    boxShadowRadius: 6,
+    boxShadowOpacity: 0.26,
+    marginTop: 20,
+  },
+  image: {},
+  errorStyle: {},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
